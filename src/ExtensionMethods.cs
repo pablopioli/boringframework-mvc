456a183e68b569bccb4c0249946aa5ea7a967f35
Pablo Pioli <ppioli@outlook.com>
2019-11-07 10:02:34 -0300

    Add user readable message for ValidationProblemDetails

---
+++ src/ExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Text;
+
+namespace Boring
+{
+    public static class ExtensionMethods
+    {
+        public static string GetUserDisplayableDescription(this ValidationProblemDetails problem)
+        {
+            string problemDesc;
+
+            if (problem.Errors != null && problem.Errors.Count > 0)
+            {
+                problemDesc = problem.Errors.Aggregate(
+                                  new StringBuilder(),
+                                  (current1, next1) => current1.Append(current1.Length == 0 ? "" : "<br>").Append(
+                                     next1.Value.Aggregate(new StringBuilder(),
+                                     (current2, next2) => current2.Append(current2.Length == 0 ? "" : "<br>").Append(
+                                         next2 + (next2.EndsWith(".") ? "" : ".")))
+                                  ))
+                    .ToString();
+            }
+            else
+            {
+                problemDesc = problem.Title;
+            }
+
+            return problemDesc;
+        }
+    }
+}
