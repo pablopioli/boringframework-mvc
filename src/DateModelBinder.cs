77bff7738efec72e64f4a43ea3c4dba8a1bc1ec5
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-06-04 23:53:53 -0300

    Add DateModelBinder

---
+++ src/DateModelBinder.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Threading.Tasks;
+
+namespace Boring.Mvc
+{
+    public class DateModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            if (bindingContext.ModelType != typeof(Date))
+            {
+                return Task.CompletedTask;
+            }
+
+            var modelName = GetModelName(bindingContext);
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var dateToParse = valueProviderResult.FirstValue;
+
+            if (string.IsNullOrEmpty(dateToParse))
+            {
+                return Task.CompletedTask;
+            }
+
+            var date = Date.Parse(dateToParse);
+            bindingContext.Result = ModelBindingResult.Success(date);
+
+            return Task.CompletedTask;
+        }
+
+        private string GetModelName(ModelBindingContext bindingContext)
+        {
+            if (!string.IsNullOrEmpty(bindingContext.BinderModelName))
+            {
+                return bindingContext.BinderModelName;
+            }
+
+            return bindingContext.ModelName;
+        }
+    }
+}
---
+++ src/DateModelBinderProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Boring.Mvc
+{
+    public class DateModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType == typeof(Date))
+            {
+                return new BinderTypeModelBinder(typeof(DateModelBinder));
+            }
+
+            return null;
+        }
+    }
+}
